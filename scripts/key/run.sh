5c2290a8bc2e0cc2b80a4c581d193078b0fe44be
Hoang Truong Phuoc <phuocht@hiworld.com.vn>
2022-04-15 18:09:32 +0700

    assignment4

---
+++ scripts/key/run.sh
@@ -0,0 +1,8 @@
+curl --request POST --url 'http://localhost:9933' -H 'Content-Type: application/json' -d @Aura1
+curl --request POST --url 'http://localhost:9933' -H 'Content-Type: application/json' -d @Gran1
+
+curl --request POST --url 'http://localhost:9933' -H 'Content-Type: application/json' -d @Aura2
+curl --request POST --url 'http://localhost:9933' -H 'Content-Type: application/json' -d @Gran2
+
+curl --request POST --url 'http://localhost:9933' -H 'Content-Type: application/json' -d @Aura3
+curl --request POST --url 'http://localhost:9933' -H 'Content-Type: application/json' -d @Gran3
